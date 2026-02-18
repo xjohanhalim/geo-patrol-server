7b5e00d69fa09f23922fff9118390f253bb75b8a
xjohanhalim <148331167+xjohanhalim@users.noreply.github.com>
2026-02-18 19:45:34 +0700

    Update server.js

--- server.js
+++ server.js
@@ -211,6 +211,38 @@ app.get('/', (req, res) => {
   res.send('API GEO PATROL RUNNING');
 });
 
+app.get('/init-db', async (req, res) => {
+  try {
+
+    await db.query(`
+      CREATE TABLE IF NOT EXISTS users (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(50) UNIQUE,
+        password_hash VARCHAR(255),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+    `);
+
+    await db.query(`
+      CREATE TABLE IF NOT EXISTS laporan_pengiriman (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        id_kurir INT,
+        no_resi VARCHAR(100),
+        foto_path VARCHAR(255),
+        latitude VARCHAR(50),
+        longitude VARCHAR(50),
+        status VARCHAR(20),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+    `);
+
+    res.send("Database initialized successfully 🚀");
+
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
 // ==========================
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
